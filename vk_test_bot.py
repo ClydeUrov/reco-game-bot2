0b536c16e8ad278bf3e6cd72a7af0cf1f7e5f33e
Владислав Юров <clydeurov@gmail.com>
2022-11-11 11:55:37 +0200

    Added DialogFlow to VK bot

--- vk_test_bot.py
+++ vk_test_bot.py
@@ -1,5 +1,6 @@
 import os
 import vk_api
+import vk_api as vk
 import random
 from dotenv import load_dotenv
 from vk_api.longpoll import VkLongPoll, VkEventType
@@ -7,6 +8,7 @@ from requests.adapters import HTTPAdapter
 import logging
 import telegram
 from detect_intent import detect_intent_text
+from pprint import pprint
 
 load_dotenv()
 
@@ -24,48 +26,28 @@ class TelegramLogsHandler(logging.Handler):
 logger = logging.getLogger('Logger')
 
 
-def send_message_vk(text, vk_api, session_id):
-    vk_api.messages.send(user_id=session_id, message=text, random_id=random.randint(1, 1000))
+
+def echo(event, vk_api):
+    project_id = os.environ['PROJECT_ID']
+    intent = detect_intent_text(
+        project_id=project_id,
+        session_id=f'vk-{event.user_id}',
+        text=event.text,
+        language_code="ru"
+    )
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=intent,
+        random_id=random.randint(1,1000)
+    )
 
 
-def send_answer_to_vk(project_id, text, session_id, language_code, vk_api):
-    answer = detect_intent_text(project_id, f'vk-{session_id}', text, language_code)
-    if answer.query_result.intent.display_name != 'Default Fallback Intent':
-        send_message_vk(answer.query_result.fulfillment_text, vk_api, session_id)
-
-
-def send_log_message(tg_token, tg_chat_id, text):
-    tg_bot.send_message(tg_chat_id, text)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     load_dotenv()
     vk_token = os.environ['VK_TOKEN']
-    project_id = os.environ['PROJECT_ID']
-    tg_chat_id = os.environ['TG_CHAT_ID']
-    tg_token = os.environ['TG_TOKEN']
-
-
-    vk_session = vk_api.VkApi(token=vk_token)
-    vk_session.http.proxies = {
-        'http': 'http://127.0.0.1:8080/',
-        'https': 'https://127.0.0.1:8080/'
-    }
+    vk_session = vk.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
-
-    tg_bot = telegram.Bot(token=tg_token)
-
-    logging.basicConfig(level=10)
-    logger.setLevel(logging.INFO)
-    logger.addHandler(TelegramLogsHandler(tg_bot, tg_chat_id))
-
-    while True:
-        try:
-            logger.warning('VK бот запущен')
-            longpoll = VkLongPoll(vk_session)
-            for event in longpoll.listen():
-                if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-                    send_answer_to_vk(project_id, event.text, event.user_id, 'RU', vk_api)
-        except Exception as err:
-            logger.error('VK бот упал с ошибкой:')
-            logger.error(err, exc_info=True)
+    longpoll = VkLongPoll(vk_session)
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
